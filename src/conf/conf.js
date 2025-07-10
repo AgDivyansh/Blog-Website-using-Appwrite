f3b88bab61d0febb0a807e0b5b6fff8109ebd9e9
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-07-10 14:24:02 +0530

    created the auth.js which includes all the functionality of login, logout, create account

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatanaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    qppwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+
+export default conf ;
